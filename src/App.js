bf0176e9c9254a635242b84ffc611a9abb799289
qkrwnstn356 <qkrwnstn345@gmail.com>
2024-11-20 10:08:18 +0900

    game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 // import Header from './components/Header';
 // import Footer from './components/Footer';
 import Main from './components/Main';
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    // element: <Layout />, // 공통 레이아웃
-    children: [
-      { path: "/", element: <Main/> },
-    ],
-  },
-]);
 
 function App() {
 
   return (<>
     <div>
-      <RouterProvider router={router} /> {/* 라우터 제공 */}
+      <Main></Main>
     </div>
   </>);
 }
